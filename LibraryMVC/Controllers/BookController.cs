aa1d57d2bb99d6d8e6ca74c7914d81f2a5e25e97
faspen <aspensonf@gmail.com>
2024-09-23 10:37:23 -0500

    edit and delete for book

--- LibraryMVC/Controllers/BookController.cs
+++ LibraryMVC/Controllers/BookController.cs
@@ -77,5 +77,86 @@ namespace LibraryMVC.Controllers
 
             return View(book);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var book = _repository.GetBook(id);
+            if (book == null)
+            {
+                return View("Error");
+            }
+
+            var dto = _mapper.Map<BookAddEditDto>(book);
+            var authors = _authorRepository.GetAuthors();
+            var genres = _genreRepository.GetGenres();
+            ViewBag.Authors = new SelectList(authors, "Id", "LastName");
+            ViewBag.Genres = new SelectList(genres, "Id", "Name");
+
+            return View(dto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(BookAddEditDto dto)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!_repository.BookExists(dto.Id))
+                {
+                    return View("Error");
+                }
+
+                var book = _mapper.Map<Book>(dto);
+                var result = _repository.UpdateBook(book);
+
+                if (result)
+                    return RedirectToAction("Index");
+                else
+                    return StatusCode(500, ModelState);
+            }
+
+            return View("Error");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var book = _repository.GetBook(id);
+            if (book == null)
+            {
+                return View("Error");
+            }
+
+            var dto = _mapper.Map<BookAddEditDto>(book);
+            var authors = _authorRepository.GetAuthors();
+            var genres = _genreRepository.GetGenres();
+            ViewBag.Authors = new SelectList(authors, "Id", "LastName");
+            ViewBag.Genres = new SelectList(genres, "Id", "Name");
+
+            return View(dto);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(BookAddEditDto book)
+        {
+            if (!_repository.BookExists(book.Id))
+                return View("Error");
+            
+            var bookToDelete = _repository.GetBook(book.Id);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            
+            if (!_repository.DeleteBook(bookToDelete))
+            {
+                ModelState.AddModelError("", "Something went wrong while deletign.");
+                return StatusCode(500, ModelState);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
